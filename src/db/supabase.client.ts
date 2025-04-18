60bbe5c81cdd7a908a3a8e985b06bd8013839235
kjabcon <kjabcon@intercars.eu>
2025-04-18 12:15:27 +0200

    feat: generations api

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -5,4 +5,6 @@ import type { Database } from "./database.types";
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
-export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey); 
+export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export const DEFAULT_USER_ID = "c4f9e3dc-5a6d-431a-8ae2-8176fdfe0267";
